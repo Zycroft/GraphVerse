a4c5895c38796f4c0386014a6eadc5061f25d6d7
ParkerWilliams <parker.williams@gmail.com>
2024-08-07 14:26:38 -0700

    strange workaround to get things back in sync

--- graphverse/llm/__init__.py
+++ graphverse/llm/__init__.py
@@ -2,4 +2,3 @@ from .model import WalkTransformer
 from .training import train_model
 from .token_generation import generate_walk
 from .evaluation import evaluate_model, count_rule_violations
-fixing strange broken things
