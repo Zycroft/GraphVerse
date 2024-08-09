8ae4231abd1bda24238b609a667cdcd7272baae5
Parker Williams <parker.williams@gmail.com>
2024-08-09 12:12:05 -0700

    init fix

--- graphverse/graph/__init__.py
+++ graphverse/graph/__init__.py
@@ -1,3 +1,3 @@
-from .graph_generation import generate_graph, calculate_edge_density
+from .graph_generation import generate_random_graph, calculate_edge_density
 from .rules import define_ascenders, define_descenders, define_evens_odds, check_rule_compliance
 from .walk import generate_valid_walk, generate_multiple_walks
